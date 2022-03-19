7a6861ac20873608b102f857a459f5204e9cac06
dardachofa <92189457+dardachofa@users.noreply.github.com>
2022-03-18 20:01:48 -0600

    Add files via upload

---
+++ beastdays/sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var merlin, aguita, amanojaku, ballenafantasma, basan, boladefuego, dragon, esfinge, fenix, fondo, grifo, 
+hidra, manticora, medusa, ogro, picodenieve, plataformaflotante, podersemillas, quilin, quimera, silfide,
+sirena, tierracompleta, unicornio, zorrodemuchascolas;
+
+
+function preload(){
+
+
+
+}
+
+function setup(){
+ 
+ 
+    createCanvas(1200,400);
+    background("imagenes/fondo.png");
+
+
+}
+
+function draw(){
+
+
+
+drawSprites;
+}
+
+function display(){
+
+
+}
